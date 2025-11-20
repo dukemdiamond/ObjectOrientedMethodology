67728e2344d35da0cf3312adec85fbb83863d225
Duke Diamond <diamond.d@northeastern.edu>
2025-11-20 15:53:09 -0500

    directories

--- Exam Practice/exam2starter-2/src/cs3500/ratings/Show.java
+++ Exam Practice/exam2starter-2/src/cs3500/ratings/Show.java
@@ -1,7 +1,6 @@
 package cs3500.ratings;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
@@ -9,25 +8,30 @@ import java.util.Objects;
  * Value class representing a television show. Shows
  * are pairs of names and their genres.
  */
-public final class Show implements Comparable<Show> {
+public final class Show implements Comparable<Show>
+{
 
   private final String name;
   private final List<Genres> genres;
 
   /**
    * Constructs a show with the given name and genres
-   * @param name the name of the show
+   *
+   * @param name   the name of the show
    * @param genres the genres of the show
    * @throws IllegalArgumentException if name or genres is null or
-   *      if genres is empty
+   *                                  if genres is empty
    */
-  public Show(String name, List<Genres> genres) {
-    if(name == null) {
+  public Show(String name, List<Genres> genres)
+  {
+    if (name == null)
+    {
       throw new IllegalArgumentException("Show name cannot be null");
     }
     this.name = name;
 
-    if(genres == null || genres.isEmpty()) {
+    if (genres == null || genres.isEmpty())
+    {
       throw new IllegalArgumentException("Genres cannot be null or empty");
     }
 
@@ -36,50 +40,65 @@ public final class Show implements Comparable<Show> {
 
   /**
    * Returns the name of the show
+   *
    * @return the name of the show
    */
-  public String name() {
+  public String name()
+  {
     return name;
   }
 
   /**
    * Returns the genres associated with the show
+   *
    * @return genres associated with the show
    */
-  public List<Genres> genres() {
+  public List<Genres> genres()
+  {
     return new ArrayList<>(genres);
   }
 
   /**
    * Returns true if and only if these are shows
    * have the same name and the same genres
+   *
    * @param other the other object to compare against.
    * @return true if and only if they represent the same show
    */
   @Override
-  public boolean equals(Object other) {
-    if (other == this) { return true; }
-    if (!(other instanceof Show)) { return false; }
+  public boolean equals(Object other)
+  {
+    if (other == this)
+    {
+      return true;
+    }
+    if (!(other instanceof Show))
+    {
+      return false;
+    }
 
-    Show that = (Show)other;
+    Show that = (Show) other;
     return this.name.equals(that.name)
         && this.genres.equals(that.genres);
   }
 
   @Override
-  public int hashCode() {
+  public int hashCode()
+  {
     return Objects.hash(this.name, this.genres);
   }
 
   /**
    * Compares two shows by name, ignoring the case.
+   *
    * @param other the object to be compared.
    * @return <0 if this < other
-   *         =0 if this == other
-   *         >0 if this > other
+   * =0 if this == other
+   * >0 if this > other
    */
   @Override
-  public int compareTo(Show other) {
+  public int compareTo(Show other)
+  {
     return this.name.compareToIgnoreCase(other.name);
   }
 }
--- Regular Expressions/RegEx/src/main/cs3500/rpgshopping/CharacterBackpack.java
+++ Regular Expressions/RegEx/src/main/cs3500/rpgshopping/CharacterBackpack.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * The backpack for some entity in the game.
  * Stores gold separately from the items.
  * The internal representation uses a list of custom
  * entries to allow for easy searching of the contents.
- *
+ * <p>
  * While a Map is more efficient, the list allows for
  * more readable code if an implementer decides to
  * search.
  */
-public class CharacterBackpack implements Backpack {
+public class CharacterBackpack implements Backpack
+{
   private double currencyHeld;
 
   /**
@@ -24,7 +24,8 @@ public class CharacterBackpack implements Backpack {
    * Wrapper around an item and how many of it exist
    * in the backpack.
    */
-  private class ContentEntry {
+  private class ContentEntry
+  {
     private Item item;
     private int amount;
 
@@ -32,16 +33,20 @@ public class CharacterBackpack implements Backpack {
      * Creates a new ContentEntry.
      * Assumes the item is non-null and every method in
      * this class prevents null items from being given.
+     *
      * @param item The item in the backpack
-     * @param amt The amount of that item in the backpack
+     * @param amt  The amount of that item in the backpack
      * @throws IllegalArgumentException if amt is negative or item is null
      */
-    ContentEntry(Item item, Integer amt) {
-      if(item == null) {
+    ContentEntry(Item item, Integer amt)
+    {
+      if (item == null)
+      {
         throw new IllegalArgumentException("item cannot be null");
       }
       this.item = item;
-      if(amt < 0) {
+      if (amt < 0)
+      {
         throw new IllegalArgumentException("Amount cannot be negative");
       }
       this.amount = amt;
@@ -54,20 +59,26 @@ public class CharacterBackpack implements Backpack {
    * Creates a backpack. This backpack has no
    * gold and no contents.
    */
-  public CharacterBackpack() {
+  public CharacterBackpack()
+  {
     this.contents = new ArrayList<>();
     this.currencyHeld = 0.0;
   }
 
   @Override
-  public void addItemToInventory(Item item, int amt) throws IllegalArgumentException {
-    if(item == null) {
+  public void addItemToInventory(Item item, int amt) throws IllegalArgumentException
+  {
+    if (item == null)
+    {
       throw new IllegalArgumentException("item cannot be null");
     }
 
-    for(ContentEntry entry : contents) {
-      if(entry.item.equals(item)) {
-        if(entry.amount + amt < 0) {
+    for (ContentEntry entry : contents)
+    {
+      if (entry.item.equals(item))
+      {
+        if (entry.amount + amt < 0)
+        {
           throw new IllegalArgumentException("amt to remove cannot exceed the amount in backpack");
         }
         entry.amount += amt;
@@ -75,33 +86,41 @@ public class CharacterBackpack implements Backpack {
       }
     }
 
-    if(amt < 0) {
-      throw new IllegalArgumentException("amount cannot be negative if none of this item is in backpack");
+    if (amt < 0)
+    {
+      throw new IllegalArgumentException(
+          "amount cannot be negative if none of this item is in backpack");
     }
     contents.add(new ContentEntry(item, amt));
   }
 
   @Override
-  public void addCurrency(double currency) {
-    if(currency < 0 && this.currencyHeld < Math.abs(currency)) {
+  public void addCurrency(double currency)
+  {
+    if (currency < 0 && this.currencyHeld < Math.abs(currency))
+    {
       throw new IllegalArgumentException("Cannot remove more currency than is in the backpack");
     }
     this.currencyHeld += currency;
   }
 
   @Override
-  public double backpackWorth() {
+  public double backpackWorth()
+  {
     double worth = 0;
-    for(ContentEntry entry : contents) {
+    for (ContentEntry entry : contents)
+    {
       worth += (entry.item.price() * entry.amount);
     }
     return worth + currencyHeld;
   }
 
   @Override
-  public Map<Item, Integer> contents() {
+  public Map<Item, Integer> contents()
+  {
     Map<Item, Integer> result = new HashMap<>();
-    for(ContentEntry entry : contents) {
+    for (ContentEntry entry : contents)
+    {
       result.put(entry.item, entry.amount);
     }
 
